599d4275f6ef92e9606b618a0802d758fa7edbfb
Oluwadamilola Olaniyi <96773767+Oludammydy@users.noreply.github.com>
2023-03-03 06:24:13 +0100

    Fixed error

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,16 +1,10 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-if (process.stdin.isTTY) {
-  process.stdin.on('data', (data) => {
-    process.stdout.write(`Your name is: ${data.toString()}`);
-    process.exit();
-  });
-} else {
-  process.stdin.on('data', (data) => {
-    process.stdout.write(`Your name is: ${data.toString()}`);
-    process.exit();
-  });
-  process.on('exit', () => {
-    process.stdout.write('This important software is now closing\n');
-  });
-}
+readline.question(`Welcome to Holberton School, what is your name?"\n`, name => {
+  console.log(`Your name is: ${name}`);
+  console.log("This important software is now closing");
+  readline.close();
+})
